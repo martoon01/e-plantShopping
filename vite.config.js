ed27d14ccdb7b5605f71d736e2ff885a449614a0
martoon01 <martoonsherpa01@gmail.com>
2025-02-14 21:47:26 +1100

    deploy fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  },
+  base: "/e-plantShopping",
   plugins: [react()],
+  optimizeDeps: {
+    include: [
+      'react',
+      'react-dom',
+      'react-redux',
+      '@reduxjs/toolkit'
+    ]
+  },
 })
